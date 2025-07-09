092aad6bdb0eb87908f88c9a8703c465841a658c
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-07-09 11:20:47 -0400

    Se agrega Logger en Interceptor, Clase configuración con registro del interceptor

--- src/main/java/com/jmachuca/curso/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/jmachuca/curso/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,7 @@
 package com.jmachuca.curso.springboot.app.interceptor.springboot_interceptor.interceptors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,16 +9,19 @@ import org.springframework.web.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
+@Component("timeInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor{
+
+    private static final Logger logger = LoggerFactory.getLogger(LogerTimeInterceptor.class);
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-  
+        logger.info("LoadingTimeInterceptor: postHandle() Terminando...");
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("LoadingTimeInterceptor: preHandle() Iniciando...");
         return true;
     }
 
